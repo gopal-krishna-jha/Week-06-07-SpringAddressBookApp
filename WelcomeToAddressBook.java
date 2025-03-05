69e23cd71b43c3bcf917bc703cf4ad5e8edadd0f
Gopal Krishna Jha <141767531+gopal-krishna-jha@users.noreply.github.com>
2025-03-05 11:33:47 +0530

    [Gopal Krishna Jha] Add Welcome file.
    [Gopal Krishna Jha] Add Welcome file.

---
+++ WelcomeToAddressBook.java
@@ -0,0 +1,6 @@
+//Create a WelcomeToAddressBook to print the welcome statement
+public class WelcomeToAddressBook {
+    public static void main(String[] args) {
+        System.out.println("\"Welcome to the Address Book App!\"");
+    }
+}
